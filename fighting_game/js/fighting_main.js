73fd4f70390c0bb5ac89e674432ec67ca15a6f94
nishi240masa <masa2401106@gmail.com>
2024-06-03 09:23:09 +0900

    Fix script source path in fighting_main.html

--- fighting_game/js/fighting_main.js
+++ fighting_game/js/fighting_main.js
@@ -1,9 +1,3 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const title = document.getElementById("title_name");
-  console.log(title);
-  title.innerText = "Fighting Game";
-});
-
 document.getElementById("return").addEventListener("click", () => {
   window.location.href = "../../main/html/index.html";
 });
